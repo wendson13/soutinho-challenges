5960d657144e2823aaa64f2fcbbbf7179e9e3371
wendson13 <wendson.dev@gmail.com>
2023-02-03 15:45:43 -0300

    fix(theme): include theme types on index

--- packages/ui/theme/index.ts
+++ packages/ui/theme/index.ts
@@ -1,16 +1,37 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      primary: {
+        "050": string;
+        500: string;
+        300: string;
+      };
+
+      neutral: {
+        "050": string;
+        900: string;
+        400: string;
+        600: string;
+      };
+    };
+  }
+}
+
 export const theme = {
   colors: {
     primary: {
-      '050': '#FFE8D9',
-      500: '#F35627',
-      300: '#FF9466',
+      "050": "#FFE8D9",
+      500: "#F35627",
+      300: "#FF9466",
     },
 
     neutral: {
-      '050': '#F7F7F7',
-      900: '#222222',
-      400: '#9E9E9E',
-      600: '#626262',
+      "050": "#F7F7F7",
+      900: "#222222",
+      400: "#9E9E9E",
+      600: "#626262",
     },
   },
 };
